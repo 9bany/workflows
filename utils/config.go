853cdce8d878e33baee9233d0feb8d27a223bd66
9bany <bany.y0599@gmail.com>
2023-05-21 17:23:47 +0700

    utils: add config

---
+++ utils/config.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"os"
+)
+
+type Config struct {
+	AppToken string `mapstructure:"SLACK_APP_TOKEN"`
+	BotToken string `mapstructure:"SLACK_BOT_TOKEN"`
+}
+
+func LodConfig(path string) Config {
+	return Config{
+		AppToken: os.Getenv("SLACK_APP_TOKEN"),
+		BotToken: os.Getenv("SLACK_BOT_TOKEN"),
+	}
+
+}
